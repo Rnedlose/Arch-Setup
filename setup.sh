de80b36486e995b3a7993b6a067144fa795a8b19
Rodney Nedlose <nedloserodney@gmail.com>
2025-09-20 19:29:29 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,27 +4,17 @@ set -e # Exit on any error
 
 echo "Starting Arch Linux package installation..."
 
-# Update system first
 echo "Updating system..."
 sudo pacman -Syu --noconfirm
 
-# Install yay AUR helper if not already installed
-if ! command -v yay &>/dev/null; then
-  echo "Installing yay AUR helper..."
-  cd /tmp
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  makepkg -si --noconfirm
-  cd ~
-else
-  echo "yay is already installed, skipping..."
-fi
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ~
 
-# Install packages from official repositories
 echo "Installing packages from official repositories and the AUR..."
 yay -S --noconfirm \
   swayfx \
-  sway-scrathpad-git \
   autotiling \
   neovim \
   ripgrep \
@@ -93,48 +83,25 @@ yay -S --noconfirm \
 
 echo "All packages have been installed successfully!"
 
-# Installing LazyVim
-mv ~/.config/nvim{,.bak}
-mv ~/.local/share/nvim{,.bak}
-mv ~/.local/state/nvim{,.bak}
-mv ~/.cache/nvim{,.bak}
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 
-# Install tmux plugin manager
-if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-  echo "Installing tmux plugin manager..."
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-else
-  echo "tmux plugin manager already installed, skipping..."
-fi
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Install tokyonight gtk theme
-if [ ! -d "$HOME/Tokyo-Night-GTK-Theme" ]; then
-  echo "Installing Tokyo-Night-GTK-Theme..."
-  git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme.git ~/Tokyo-Night-GTK-Theme
-  cd Tokyo-Night-GTK-Theme/themes
-  ./install.sh -c dark -t default --tweaks storm -l
-  gsettings set org.gnome.desktop.interface gtk-theme 'Tokyonight-Dark-Storm'
-  gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
-else
-  echo "Theme already installed, skipping..."
-fi
+git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme.git ~/Tokyo-Night-GTK-Theme
+cd Tokyo-Night-GTK-Theme/themes
+./install.sh -c dark -t default --tweaks storm -l
+gsettings set org.gnome.desktop.interface gtk-theme 'Tokyonight-Dark-Storm'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 
-# dotfiles
-if [ ! -d "$HOME/dotfiles" ]; then
-  echo "Cloning dotfiles..."
-  git clone https://github.com/Rnedlose/dotfiles.git ~/dotfiles
-  echo "Ensuring clean directories for stow..."
-  rm -rf ~/.config/hypr ~/.config/kitty 2>/dev/null || true
-  echo "Setting symlinks with stow..."
-  cd ~/dotfiles
-  if stow .; then
-    echo "Stow completed successfully."
-  else
-    echo "Warning: Stow encountered issues, you need to run it manually."
-fi
+git clone https://github.com/Rnedlose/dotfiles.git ~/dotfiles
+echo "Ensuring clean directories for stow..."
+echo "Setting symlinks with stow..."
+cd ~/dotfiles
+stow .
 
 sudo systemctl enable ly.service
 sudo systemctl start ly.service
 
-echo "You should probably reboot now..."
+echo "Rebooting now"
+
+sudo reboot now
